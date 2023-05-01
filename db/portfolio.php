99330ef47b45459a3753ac872467b50c7410e8ef
hangocmy <hangocmy00@gmail.com>
2023-05-01 11:55:09 +0700

    upgrade

--- db/portfolio.php
+++
@@ -1,9 +0,0 @@
-<?php
-  $host = 'mysql:host=127.0.0.1;dbname=portfolio';
-  $username = 'root';
-  $password = '';
-
-  $db = new MongoDB\Client('mongodb+srv://hangocmy:bemi@1909@cluster0.vzqufnc.mongodb.net/?retryWrites=true&w=majority');
-
-  //$db = new MongoClient($host, $username, $password);
-?>
